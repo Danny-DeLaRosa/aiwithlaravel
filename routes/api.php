ca6dbf6a3bdf502efb785a890c7f538480108893
Danny De La Rosa <danny.devlarosa@gmail.com>
2024-02-06 11:20:13 -0800

    api routes file for side project

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Render this in postman, JSON object. covert from message "hello world" to response from open ai.
+// Google: Laravel API structure and Laravel RESTful APIs.
+// Pass something to the function to have a dynamic parameter (passing postbodies to laravel api routes)
+Route::get('hello_world', function () {
+    return response()->json(['message' => 'Hello World']);
+});
+
+
+// DONT DO ANYTHING IN MIDDLEWARE ROUTE!!!!
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
